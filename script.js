cd83bbe75f2d46b701294b463e2018b7af2db780
Muhammmed Rashid V <159544833+rashidmdv@users.noreply.github.com>
2024-03-02 21:10:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -92,11 +92,17 @@ testimonialsSwiperEl.initialize();
 
 
 
-// /*--    
-//     Counter Up
-//   -----------------------------------*/
+/*--    
+    Counter Up
+  -----------------------------------*/
 
 // $('.counter').counterUp({
 //     delay: 10,
 //     time: 1500,
-// });
+// })
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000,
+    });
+});
